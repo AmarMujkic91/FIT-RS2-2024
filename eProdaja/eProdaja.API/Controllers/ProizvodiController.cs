91cf0b5e652823792e2d030f7f51ccb1d1db1ec1
AmarMujkic <amar.mujkic@edu.fit.ba>
2024-05-27 22:36:28 +0200

    Prvi Commit

---
+++ eProdaja/eProdaja.API/Controllers/ProizvodiController.cs
@@ -0,0 +1,24 @@
+﻿using eProdaja.Model;
+using eProdajaServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eProdaja.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodiController : ControllerBase
+    {
+        protected IProizvodiServices _dbService;
+
+        public ProizvodiController(IProizvodiServices dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public List<Proizvodi> GetList()
+        {
+            return _dbService.GetList();
+        }
+    }
+}
---
+++ eProdaja/eProdajaServices/ProizvodiService.cs
@@ -0,0 +1,33 @@
+﻿using eProdaja.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdajaServices
+{
+    public class ProizvodiService : IProizvodiServices
+    {
+        List<Proizvodi> list = new List<Proizvodi>()
+        {
+            new Proizvodi()
+            {
+                ProizvodId = 1,
+                Naziv = "Laptop",
+                Cijena = 1099
+            },
+            new Proizvodi()
+            {
+                ProizvodId = 2,
+                Naziv = "Tablet",
+                Cijena = 56
+            }
+        };
+
+        public List<Proizvodi> GetList()
+        {
+            return list;
+        }
+    }
+}
